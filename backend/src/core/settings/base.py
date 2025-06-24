0297f89d03dd5c2b8a6f53895b5d9a2351532483
aldick <aldiyarbakibek@gmail.com>
2025-06-24 19:10:09 +0000

    cors headers added

--- backend/src/core/settings/base.py
+++ backend/src/core/settings/base.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
     "djoser",
@@ -19,6 +20,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -90,3 +92,10 @@ DJOSER = {
         'user_create': 'account.serializers.CustomUserSerializer',
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
