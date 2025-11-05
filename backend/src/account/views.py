e876caab8a7d2634e9898d3eee75d741d7931330
aldick <aldiyarbakibek1205@gmail.com>
2025-11-05 08:58:14 +0500

    added check of is user is authenticated

--- backend/src/account/views.py
+++ backend/src/account/views.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
 
 from .serializers import DepositSerializer
@@ -65,8 +64,6 @@ class LogoutView(APIView):
     
 
 class DepositView(APIView):
-    permission_classes = [IsAuthenticated]
-    
     def get(self, request):
         return Response({
             "balance": request.user.balance
